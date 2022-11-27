3fd5b21c65eda01fe1701564ba930686226c94a0
Joe Milne <jwhmilne@gmail.com>
2022-11-27 18:47:26 +0000

    Half finish exercise two

--- 015-maps/exercises/exercise-2.js
+++ 015-maps/exercises/exercise-2.js
@@ -24,3 +24,82 @@
 
     9. Print out both "store" and "candy"
 */
+
+const banana = {
+  name: "banana",
+  quantity: 1,
+  price: 1.95,
+};
+
+const apple = {
+  name: "apple",
+  quantity: 1,
+  price: 1.45,
+};
+
+const candy = {
+  name: "candy",
+  quantity: 1,
+  price: 3.5,
+};
+
+const store = new Map();
+store.set("storeNumber", 5);
+store.set("locationCity", "Milan");
+store.set("locationCountry", "Italy");
+store.set("products", [banana, apple, candy]);
+
+console.log(store);
+// Map(4) {
+//     'storeNumber' => 5,
+//     'locationCity' => 'Milan',
+//     'locationCountry' => 'Italy',
+//     'products' => [
+//       { name: 'banana', quantity: 1, price: 1.95 },
+//       { name: 'apple', quantity: 1, price: 1.45 },
+//       { name: 'candy', quantity: 1, price: 3.5 }
+//     ]
+//   }
+
+console.log(store.get("products"));
+// [
+//     { name: 'banana', quantity: 1, price: 1.95 },
+//     { name: 'apple', quantity: 1, price: 1.45 },
+//     { name: 'candy', quantity: 1, price: 3.5 }
+//   ]
+
+console.log(store.get("products")[2]);
+// { name: 'candy', quantity: 1, price: 3.5 }
+
+store.get("products")[0].price = 1.75;
+console.log(store);
+// Map(4) {
+//     'storeNumber' => 5,
+//     'locationCity' => 'Milan',
+//     'locationCountry' => 'Italy',
+//     'products' => [
+//       { name: 'banana', quantity: 1, price: 1.75 },
+//       { name: 'apple', quantity: 1, price: 1.45 },
+//       { name: 'candy', quantity: 1, price: 3.5 }
+//     ]
+//   }
+
+console.log(banana);
+// { name: 'banana', quantity: 1, price: 1.75 }
+
+candy.price = 4.99;
+
+console.log(store);
+// Map(4) {
+//     'storeNumber' => 5,
+//     'locationCity' => 'Milan',
+//     'locationCountry' => 'Italy',
+//     'products' => [
+//       { name: 'banana', quantity: 1, price: 1.75 },
+//       { name: 'apple', quantity: 1, price: 1.45 },
+//       { name: 'candy', quantity: 1, price: 4.99 }
+//     ]
+//   }
+
+console.log(candy);
+// { name: 'candy', quantity: 1, price: 4.99 }
