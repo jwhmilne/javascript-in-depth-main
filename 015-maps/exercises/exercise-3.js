958e478bd453ece51746734b2d0449418e9dae89
Joe Milne <jwhmilne@gmail.com>
2022-11-27 22:56:07 +0000

    Finish exercise 3

--- 015-maps/exercises/exercise-3.js
+++ 015-maps/exercises/exercise-3.js
@@ -21,3 +21,54 @@
 
     *This is a challenging exercise - take it slow and step by step
 */
+
+// Step 1:
+const houseForSale = new Map();
+houseForSale.set("area", 940);
+houseForSale.set("value", 320000);
+houseForSale.set("streetName", "Fifth Street");
+houseForSale.set("built", "2012");
+houseForSale.set("owner", { name: "Blake", age: 29 });
+houseForSale.set("offers", [290000, 295000, 315000, 312000]);
+
+// Step 2:
+console.log(houseForSale);
+// Map(6) {
+//     'area' => 940,
+//     'value' => 320000,
+//     'streetName' => 'Fifth Street',
+//     'built' => '2012',
+//     'owner' => { name: 'Blake', age: 29 },
+//     'offers' => [ 290000, 295000, 315000, 312000 ]
+//   }
+
+// Step 3:
+houseForSale.delete("built");
+
+// Step 4:
+houseForSale.get("owner").age = 30;
+
+// Step 5:
+console.log(
+  houseForSale.get("offers").reduce((prev, curr) => {
+    if (prev > curr) {
+      return prev;
+    }
+    return curr;
+  }, 0)
+);
+// 315000
+
+// Step 6:
+houseForSale.set("sale price", 312000);
+
+// Step 7:
+console.log(houseForSale);
+// Map(6) {
+//     'area' => 940,
+//     'value' => 320000,
+//     'streetName' => 'Fifth Street',
+//     'owner' => { name: 'Blake', age: 30 },
+//     'offers' => [ 290000, 295000, 315000, 312000 ],
+//     'sale price' => 312000
+//   }
