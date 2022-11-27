955c06cf8be6ccef0ef485714bacc701792ea63b
Joe Milne <jwhmilne@gmail.com>
2022-11-27 16:44:35 +0000

    Finish first exercise

---
+++ 015-maps/exercises/warmup-exercise.js
@@ -0,0 +1,39 @@
+/*
+    1. Create a Map called "mexico" with the following entries:
+        - id ➔ 24
+        - name ➔ "Mexico"
+        - capital ➔ "Mexico City"
+        - neighbours ➔ ["USA", "Guatemala", "Belize"]
+
+    2. Print out "mexico" to the terminal
+
+    3. Change "id" to be 25
+
+    4. Add "Honduras" to the list of neighbours to "mexico"
+
+    5. Print out "mexico" to the terminal again
+*/
+
+const mexico = new Map();
+mexico.set("id", 24);
+mexico.set("name", "Mexico");
+mexico.set("capital", "Mexico City");
+mexico.set("neighbours", ["USA", "Guatemala", "Belize"]);
+console.log(mexico);
+// Map(4) {
+//     'id' => 24,
+//     'name' => 'Mexico',
+//     'capital' => 'Mexico City',
+//     'neighbours' => [ 'USA', 'Guatemala', 'Belize' ]
+//   }
+
+mexico.set("id", 25);
+mexico.get("neighbours").push("Honduras");
+
+console.log(mexico);
+// Map(4) {
+//     'id' => 25,
+//     'name' => 'Mexico',
+//     'capital' => 'Mexico City',
+//     'neighbours' => [ 'USA', 'Guatemala', 'Belize', 'Honduras' ]
+//   }
