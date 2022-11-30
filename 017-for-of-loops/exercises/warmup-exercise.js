329e737fe2ea800479d012da62ecf7d2c0aa9b5e
Joe Milne <jwhmilne@gmail.com>
2022-11-30 23:13:52 +0000

    Add warmup exercise for of loops

---
+++ 017-for-of-loops/exercises/warmup-exercise.js
@@ -0,0 +1,18 @@
+/*
+    1. Create a variable called "numbers" that points to this array:
+        [10, 20, 40, 50, 20, 100]
+
+    2. Use a for-of loop to print out each number squared
+*/
+
+const numbers = [10, 20, 40, 50, 20, 100];
+
+for (number of numbers) {
+  console.log(number ** 2);
+}
+// 100
+// 400
+// 1600
+// 2500
+// 400
+// 10000
